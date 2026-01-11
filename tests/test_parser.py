81570963c33c6c336124a4eac525bc2e525a7be5
Ronald Rink <ronald.rink@d-fens.net>
2026-01-11 20:04:32 +0100

    parser
    * add parser skeleton
    * add tests for ebnf / lark syntax

---
+++ tests/test_parser.py
@@ -0,0 +1,118 @@
+# Copyright (c) 2026 d-fens GmbH, http://d-fens.ch
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# pylint: disable=C0116
+# type: ignore
+
+"""TestParser"""
+
+import unittest
+from parameterized import parameterized
+
+from src.parser import Parser
+from src.grammar_type import GrammarType
+
+
+class TestParser(unittest.TestCase):
+    """TestParser"""
+
+    sut: Parser
+
+    def setUp(self):
+        self.sut = Parser()
+
+    def test_and_display(self):
+
+        value = "00.00"
+        self.sut = Parser(GrammarType.NUMBER)
+
+        try:
+            result = self.sut.invoke(value)
+            print(result.pretty())
+
+            result = not self.sut.is_valid(value)
+            self.assertTrue(result)
+
+        except Exception as ex:  # pylint: disable=W0718
+            self.fail(ex)
+
+    def test_single_word_ending_with_dot(self):
+
+        value = "Hello."
+        result = self.sut.is_valid(value)
+        self.assertTrue(result)
+
+    def test_single_word_fails(self):
+        """The sentence must end with an EOS."""
+
+        value = "Hello"
+
+        result = self.sut.is_valid(value)
+
+        self.assertFalse(result)
+
+    def test_two_words(self):
+
+        value = "Hello world."
+
+        result = self.sut.is_valid(value)
+
+        self.assertTrue(result)
+
+    def test_two_words_with_comma(self):
+
+        value = "Hello, world."
+
+        result = self.sut.is_valid(value)
+
+        self.assertTrue(result)
+
+    def test_two_words_with_comma_missing_space_fails(self):
+
+        value = "Hello,world."
+
+        result = self.sut.is_valid(value)
+
+        self.assertFalse(result)
+
+    def test_sth(self):
+
+        value = "Hello, world. " \
+            "This is a test sentence. " \
+            "I use the number 8.15."
+
+        result = self.sut.is_valid(value)
+
+        self.assertTrue(result)
+
+    @parameterized.expand([
+        ("valid_case", "Hello, world. 8.15.", True),
+        ("missing_eos", "Hello", False),
+        ("missing_space", "Hello,world.", False),
+        ("double_space", "Hello  world.", False),
+        ("valid_case", "Hello, world.", True),
+        ("valid_case", "Hello, world!", True),
+        ("valid_case", "Hello, world?", True),
+        ("valid_case", "Hello, world:", True),
+        ("multiple_comma", "Hello, world, last word.", True),
+        ("valid_case", "No numbers here.", True),
+        ("short_case", "8.15", False),
+        ("valid_case", "8.15.", True),
+    ])
+    def test_is_valid(self, rule, value, expected):  # NOSONAR(54144)
+
+        result = self.sut.is_valid(value)
+
+        self.assertEqual(result, expected, rule)
---
+++ tests/test_parser_number.py
@@ -0,0 +1,146 @@
+# Copyright (c) 2026 d-fens GmbH, http://d-fens.ch
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# pylint: disable=C0116
+# type: ignore
+
+"""TestParser"""
+
+import unittest
+
+from parameterized import parameterized
+
+from src.grammar_type import GrammarType
+from src.parser import Parser
+
+
+class TestParserNumber(unittest.TestCase):
+    """TestParserNumber"""
+
+    sut: Parser
+
+    def setUp(self):
+        self.sut = Parser()
+
+    def test_and_display(self):
+
+        value = "0.00"
+        self.sut = Parser(GrammarType.INT)
+
+        try:
+            result = self.sut.invoke(value)
+            print(result.pretty())
+
+            result = self.sut.is_valid(value)
+            self.assertTrue(result)
+
+        except Exception as ex:  # pylint: disable=W0718
+            self.fail(ex)
+
+    @parameterized.expand([
+        ("zero", "0", True),
+        ("zero", "+0", True),
+        ("zero", "-0", True),
+
+        ("valid_positive", "1", True),
+        ("valid_positive", "12", True),
+        ("valid_positive", "123", True),
+
+        ("valid_plus", "+1", True),
+        ("valid_plus", "+12", True),
+        ("valid_plus", "+123", True),
+
+        ("valid_negative", "-1", True),
+        ("valid_negative", "-12", True),
+        ("valid_negative", "-123", True),
+    ])
+    def test_int(self, rule, value, expected) -> None:
+
+        self.sut = Parser(GrammarType.INT)
+
+        result = self.sut.is_valid(value)
+        self.assertEqual(result, expected, rule)
+
+    @parameterized.expand([
+        ("multi_dot_fails", "12.34.56", False),
+        ("missing_dot_fails", "0", False),
+        ("missing_dot_fails", "1", False),
+        ("missing_dot_fails", "12", False),
+        ("multi_leading_zero_fails", "00.0", False),
+
+        ("zero", "0.0", True),
+        ("zero", "0.00", True),
+        ("zero", "+0.0", True),
+        ("zero", "+0.00", True),
+        ("zero", "-0.0", True),
+        ("zero", "-0.00", True),
+
+        ("valid_positive", "1.0", True),
+        ("valid_positive", "1.00", True),
+        ("valid_positive", "12.3", True),
+        ("valid_positive", "12.34", True),
+
+        ("valid_plus", "+1.0", True),
+        ("valid_plus", "+1.00", True),
+        ("valid_plus", "+12.3", True),
+        ("valid_plus", "+12.34", True),
+
+        ("valid_negative", "-1.0", True),
+        ("valid_negative", "-1.00", True),
+        ("valid_negative", "-12.3", True),
+        ("valid_negative", "-12.34", True),
+    ])
+    def test_fp(self, rule, value, expected) -> None:
+
+        self.sut = Parser(GrammarType.FP)
+
+        result = self.sut.is_valid(value)
+        self.assertEqual(result, expected, rule)
+
+    @parameterized.expand([
+        ("multi_dot_fails", "12.34.56", False),
+        ("missing_dot_is_int", "0", True),
+        ("missing_dot_is_int", "1", True),
+        ("missing_dot_is_int", "12", True),
+        ("multi_leading_zero_fails", "00.0", False),
+
+        ("zero", "0.0", True),
+        ("zero", "0.00", True),
+        ("zero", "+0.0", True),
+        ("zero", "+0.00", True),
+        ("zero", "-0.0", True),
+        ("zero", "-0.00", True),
+
+        ("valid_positive", "1.0", True),
+        ("valid_positive", "1.00", True),
+        ("valid_positive", "12.3", True),
+        ("valid_positive", "12.34", True),
+
+        ("valid_plus", "+1.0", True),
+        ("valid_plus", "+1.00", True),
+        ("valid_plus", "+12.3", True),
+        ("valid_plus", "+12.34", True),
+
+        ("valid_negative", "-1.0", True),
+        ("valid_negative", "-1.00", True),
+        ("valid_negative", "-12.3", True),
+        ("valid_negative", "-12.34", True),
+    ])
+    def test_number(self, rule, value, expected) -> None:
+
+        self.sut = Parser(GrammarType.NUMBER)
+
+        result = self.sut.is_valid(value)
+        self.assertEqual(result, expected, rule)
