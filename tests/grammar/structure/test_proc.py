2d933a1459ed60434c0cbff110913af72ce99729
Ronald Rink <ronald.rink@d-fens.net>
2026-01-18 15:42:48 +0100

    grammar proc
    * add tests
    * remove extra code

--- tests/grammar/structure/test_proc.py
+++ tests/grammar/structure/test_proc.py
@@ -14,6 +14,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 # pylint: disable=C0116
+# pylint: disable=C0301
 # type: ignore
 
 """test_proc"""
@@ -110,7 +111,8 @@ class TestProc(unittest.TestCase):
         ("multi_lower_mid_with_ws", '''\n abc. some-text more-text''', True),
         ("multi_lower_mid_with_ws", '''\n  abc. some-text more-text''', True),
         ("multi_lower_mid_with_ws", '''\n   abc. some-text more-text''', True),
-        ("multi_space", '''1 .  some-text more-text''', False),
+
+        ("multi_space_will_push_space_into_line", '''1 .  some-text more-text''', True),
 
         ("alpha_num_mid", '''\n1a. some-text more-text''', True),
         ("alpha_num_mid_with_ws", '''\n 1a. some-text''', True),
@@ -126,9 +128,22 @@ class TestProc(unittest.TestCase):
         result = Parser(GrammarType.STRUCTURE).is_valid(value)
         self.assertEqual(expected, result, rule)
 
-    def test_proc(self):
+    @parameterized.expand([
+        ("sof_multi", "01. first-text more-text\n  02.  next-text more-text\nend-text", True),
+        ("sof_multi", "01. first-text more-text\n  02.  next-text more-text\nend-text", True),
+        ("leading_space_multi", " 01. first-text more-text\n  02.  next-text more-text\nend-text", True),
+        ("leading_space_multi", "  01. first-text more-text\n  02.  next-text more-text\nend-text", True),
+        ("leading_crlf_multi", "\r\n01. first-text more-text\n  02.  next-text more-text\nend-text", True),
+        ("leading_crlf_multi", "\n01. first-text more-text\n  02.  next-text more-text\nend-text", True),
+        ("leading_crlf_and_space_multi", "\r\n 01. first-text more-text\n  02.  next-text more-text\nend-text", True),
+        ("leading_crlf_and_space_multi", "\n 01. first-text more-text\n  02.  next-text more-text\nend-text", True),
+    ])
+    def test_proc(self, rule, value, expected):  # NOSONAR(54144)
 
-        value = "  01. first-text more-text\n  02.  next-text more-text\nend-text"
+        _ = rule
+        _ = expected
+
+        value = " 01. first-text more-text\n  02.  next-text more-text\nend-text"
         initial = Parser(GrammarType.STRUCTURE).invoke(value)
 
         metrics = TokenMetrics()
@@ -161,133 +176,3 @@ class TestProc(unittest.TestCase):
         self.assertEqual(Token.TEXT, metrics.pop())
 
         self.assertEqual(0, len(metrics), metrics)
-
-    # def test_cite_sof(self):
-    #     """Cite at the start of the input (without a line break at the start) is valid."""
-
-    #     value = "> cite-text more-text\nend-text"
-    #     initial = Parser(GrammarType.STRUCTURE).invoke(value)
-
-    #     metrics = TokenMetrics()
-    #     transformed = StructureTransformer(metrics, log=True).transform(initial)
-    #     print(transformed.pretty())
-
-    #     # Assert type and quantity of tokens.
-    #     self.assertEqual(7, len(metrics), metrics)
-    #     self.assertEqual(1, metrics[Token.start])
-    #     self.assertEqual(3, metrics[Token.TEXT])
-    #     self.assertEqual(1, metrics[Token.NEWLINE])
-    #     self.assertEqual(1, metrics[Token.cite])
-    #     self.assertEqual(1, metrics[Token.WS])
-
-    #     # Assert order of tokens (recursively).
-    #     self.assertEqual(Token.start, metrics.pop())
-    #     self.assertEqual(Token.TEXT, metrics.pop())
-    #     self.assertEqual(Token.NEWLINE, metrics.pop())
-
-    #     self.assertEqual(Token.cite, metrics.pop())
-    #     self.assertEqual(Token.TEXT, metrics.pop())
-    #     self.assertEqual(Token.WS, metrics.pop())
-    #     self.assertEqual(Token.TEXT, metrics.pop())
-
-    #     self.assertEqual(0, len(metrics), metrics)
-
-    # def test_single(self):
-
-    #     value = "\r\n> cite-text *bold-text*\nend-text"
-    #     initial = Parser(GrammarType.STRUCTURE).invoke(value)
-
-    #     metrics = TokenMetrics()
-    #     transformed = StructureTransformer(metrics, log=True).transform(initial)
-    #     print(transformed.pretty())
-
-    #     # Assert type and quantity of tokens.
-    #     self.assertEqual(8, len(metrics), metrics)
-    #     self.assertEqual(1, metrics[Token.start])
-    #     self.assertEqual(1, metrics[Token.cite])
-    #     self.assertEqual(1, metrics[Token.bold])
-    #     self.assertEqual(1, metrics[Token.NEWLINE])
-    #     self.assertEqual(3, metrics[Token.TEXT])
-    #     self.assertEqual(1, metrics[Token.WS])
-
-    #     # Assert order of tokens (recursively).
-    #     self.assertEqual(Token.start, metrics.pop())
-    #     self.assertEqual(Token.TEXT, metrics.pop())
-    #     self.assertEqual(Token.NEWLINE, metrics.pop())
-
-    #     self.assertEqual(Token.cite, metrics.pop())
-    #     self.assertEqual(Token.bold, metrics.pop())
-    #     self.assertEqual(Token.TEXT, metrics.pop())
-    #     self.assertEqual(Token.WS, metrics.pop())
-    #     self.assertEqual(Token.TEXT, metrics.pop())
-
-    #     self.assertEqual(0, len(metrics), metrics)
-
-    # def test_empty(self):
-    #     """An 'empty' cite line must contain a minimum of one WS."""
-
-    #     value = ">  \n>  \n"
-    #     initial = Parser(GrammarType.STRUCTURE).invoke(value)
-
-    #     metrics = TokenMetrics()
-    #     transformed = StructureTransformer(metrics, log=True).transform(initial)
-    #     print(transformed.pretty())
-
-    #     # Assert type and quantity of tokens.
-    #     self.assertEqual(6, len(metrics), metrics)
-    #     self.assertEqual(1, metrics[Token.start])
-    #     self.assertEqual(2, metrics[Token.cite])
-    #     self.assertEqual(1, metrics[Token.NEWLINE])
-    #     self.assertEqual(2, metrics[Token.WS])
-
-    #     # Assert order of tokens (recursively).
-    #     self.assertEqual(Token.start, metrics.pop())
-    #     self.assertEqual(Token.NEWLINE, metrics.pop())
-
-    #     self.assertEqual(Token.cite, metrics.pop())
-    #     self.assertEqual(Token.WS, metrics.pop())
-    #     self.assertEqual(Token.cite, metrics.pop())
-    #     self.assertEqual(Token.WS, metrics.pop())
-
-    #     self.assertEqual(0, len(metrics), metrics)
-
-    # def test_multi(self):
-
-    #     # value = "\r\n> block-quote-text1 *more-text*\n> block-quote-text1 *more-text*\nend-text"
-    #     value = "\r\n> first-text *some-text*\n> next-text 'more-text'\nend-text"
-    #     initial = Parser(GrammarType.STRUCTURE).invoke(value)
-
-    #     metrics = TokenMetrics()
-    #     transformed = StructureTransformer(metrics, log=True).transform(initial)
-    #     print(transformed.pretty())
-
-    #     # Assert type and quantity of tokens.
-    #     self.assertEqual(13, len(metrics), metrics)
-    #     self.assertEqual(1, metrics[Token.start])
-    #     # self.assertEqual(2, metrics[Token.cite])
-    #     self.assertEqual(1, metrics[Token.bold])
-    #     self.assertEqual(1, metrics[Token.squote])
-    #     self.assertEqual(1, metrics[Token.NEWLINE])
-    #     self.assertEqual(5, metrics[Token.TEXT])
-    #     self.assertEqual(2, metrics[Token.WS])
-
-    #     # Assert order of tokens (recursively).
-    #     self.assertEqual(Token.start, metrics.pop())
-    #     self.assertEqual(Token.TEXT, metrics.pop())
-    #     self.assertEqual(Token.NEWLINE, metrics.pop())
-
-    #     # # First block.
-    #     self.assertEqual(Token.cite, metrics.pop())
-    #     self.assertEqual(Token.squote, metrics.pop())
-    #     self.assertEqual(Token.TEXT, metrics.pop())
-    #     self.assertEqual(Token.WS, metrics.pop())
-    #     self.assertEqual(Token.TEXT, metrics.pop())
-
-    #     # # Second block.
-    #     self.assertEqual(Token.cite, metrics.pop())
-    #     self.assertEqual(Token.bold, metrics.pop())
-    #     self.assertEqual(Token.TEXT, metrics.pop())
-    #     self.assertEqual(Token.WS, metrics.pop())
-    #     self.assertEqual(Token.TEXT, metrics.pop())
-
-    #     self.assertEqual(0, len(metrics), metrics)
