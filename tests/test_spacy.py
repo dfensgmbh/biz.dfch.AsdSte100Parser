d6e4324ee3c5f608852aff5f2b36f73d11005fe5
Ronald Rink <dfch@users.noreply.github.com>
2026-02-11 10:27:53 +0100

    spacy functionality
    * add test for spacy functionality

---
+++ tests/test_spacy.py
@@ -0,0 +1,35 @@
+# Copyright (C) 2026 Ronald Rink, d-fens GmbH, http://d-fens.ch
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# pylint: disable=C0114
+# pylint: disable=C0115
+# pylint: disable=C0116
+
+"""test_spacy"""
+
+import unittest
+
+import spacy
+
+
+class TestSpacy(unittest.TestCase):
+
+    def test_spacy(self):
+        text = """ASD-STE100 Simplified Technical English (STE for short) """ \
+            """is a controlled natural language and an international """ \
+            """standard to write technical documentation."""
+        nlp = spacy.load("en_core_web_sm")
+        doc = nlp(text)
+        print([(token.text, token.pos_, token.dep_) for token in doc])
