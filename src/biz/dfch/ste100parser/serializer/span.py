4552bc970474cb3bbf381d6fceae3e41931fa227
Ronald Rink <dfch@users.noreply.github.com>
2026-02-15 16:58:41 +0100

    Span
    * add Span object for Interpreter

---
+++ src/biz/dfch/ste100parser/serializer/span.py
@@ -0,0 +1,38 @@
+# Copyright (C) 2026 Ronald Rink, d-fens GmbH, http://d-fens.ch
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# pylint: disable=C0103
+# pylint: disable=C0116
+# pylint: disable=W0212
+
+"""Span class."""
+
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Span:
+    """This class describes the location and length of a textual object."""
+    line: int
+    column: int
+    start_pos: int
+    end_pos: int
+
+    def __str__(self) -> str:
+        return f"[{self.line}:{self.column}] @ " \
+            f"[{self.start_pos}:{self.end_pos}]"
+
+    def __repr__(self) -> str:
+        return str(self)
